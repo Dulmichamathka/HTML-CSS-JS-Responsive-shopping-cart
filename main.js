932bb1c3fc543db4cf635f4a1440cc8daaa2a60c
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-03-08 19:17:16 +0530

    cart open close js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//cart o
+//cart open close
+
+let cartIcon = document.querySelector(".cart-icon");
+let cartModel = document.querySelector(".cart-model");
+let cartClose = document.querySelector(".close-btn");
+
+cartIcon.onclick = () => {
+  cartModel.classList.add("open-cart");
+};
+cartClose.onclick = () => {
+  cartModel.classList.remove("open-cart");
+};
